bf8c507459599f3802e4ddf6af7d75620d54a109
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-22 15:10:32 -0600

    it doesn't work

--- app/src/main/java/com/fisheradelakin/bitwear/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/bitwear/MainActivity.java
@@ -4,19 +4,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.apache.http.Header;
+import android.widget.Toast;
 import org.json.*;
-import com.loopj.android.http.*;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    public TextView mTextView;
     private Button mButton;
 
     public static final String URL = "https://api.bitcoinaverage.com/ticker/global/USD/";
@@ -29,8 +28,9 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+
                 mButton = (Button) stub.findViewById(R.id.button);
+                mTextView = (TextView) stub.findViewById(R.id.text);
 
                 mButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -38,6 +38,8 @@ public class MainActivity extends Activity {
                         // All the code for the button goes in here
                         if(isNetworkAvailable()) {
                             updatePrice();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "No network", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -46,14 +48,7 @@ public class MainActivity extends Activity {
     }
 
     protected void updatePrice() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(URL, null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                JSONObject c;
-                String price = c.getString("24h_avg");
-            }
-        });
+        new JSONParse().execute();
     }
 
     private boolean isNetworkAvailable() {
@@ -67,6 +62,34 @@ public class MainActivity extends Activity {
         }
 
         return isAvailable;
+    }
+
+    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            JSONParser jParser = new JSONParser();
+            // Getting JSON from URL
+            JSONObject json = jParser.getJsonFromUrl(URL);
+            return json;
+        }
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                // jokes = json.getJSONArray(TAG_JOKE);
+                // getting json from url
+                JSONObject c = json.getJSONObject("");
+                // store json item
+                String price = c.getString("price");
+                // set json data in textview
+                mTextView.setText(price);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
