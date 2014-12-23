7e5d9e67271364a0884cf303c28918775b6e99ae
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-23 15:07:00 -0600

    -_-

--- mobile/src/main/java/com/fisheradelakin/bitwear/MainActivity.java
+++ mobile/src/main/java/com/fisheradelakin/bitwear/MainActivity.java
@@ -11,11 +11,18 @@ import android.widget.TextView;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class MainActivity extends ActionBarActivity {
 
-    public static final String URL = "https://api.bitcoinaverage.com/ticker/global/USD/";
+    //public static final String URL = "https://api.bitcoinaverage.com/ticker/global/USD/";
     TextView mPriceText;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +30,41 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         if(isNetworkAvailable()) {
-            JSONParse parse = new JSONParse();
-            parse.execute();
+            //JSONParse parse = new JSONParse();
+            //parse.execute();
+
+            dialog();
+            update();
         }
+    }
+
+    protected void update() {
+        try {
+            URL url = new URL("https://api.bitcoinaverage.com/ticker/global/USD/24h_avg");
+            URLConnection yc = url.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            yc.getInputStream()));
+            String inputLine;
+            StringBuilder builder = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                builder.append(inputLine.trim());
+            in.close();
+            String price = builder.toString();
+            pDialog.dismiss();
+            mPriceText = (TextView) findViewById(R.id.priceTextView);
+            mPriceText.setText(price);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void dialog() {
+        pDialog = new ProgressDialog(MainActivity.this);
+        pDialog.setMessage("Getting price ...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(true);
+        pDialog.show();
     }
 
     private boolean isNetworkAvailable() {
@@ -40,7 +79,7 @@ public class MainActivity extends ActionBarActivity {
         return isAvailable;
     }
 
-    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+    /* private class JSONParse extends AsyncTask<String, String, JSONObject> {
         private ProgressDialog pDialog;
         @Override
         protected void onPreExecute() {
@@ -70,10 +109,10 @@ public class MainActivity extends ActionBarActivity {
                 String price = json.getString("24h_avg");
                 // set json data in textview
                 mPriceText = (TextView) findViewById(R.id.priceTextView);
-                mPriceText.setText(price);
+                mPriceText.setText(price.replace("n", ""));
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-    }
+    } */
 }
